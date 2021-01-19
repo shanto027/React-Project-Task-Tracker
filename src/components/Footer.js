31bbf351e35d0242d3f9d3eb082e59416c10a435
shanto halder <shantohalder63@gmail.com>
2021-01-19 18:55:56 +0600

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+
+
+const Footer = () => {
+    return (
+        <footer>
+
+            <p>Copyright &copy; Shanto Halder</p>
+            <a href='/about'>About</a>
+            
+        </footer> 
+    )
+}
+
+export default Footer
